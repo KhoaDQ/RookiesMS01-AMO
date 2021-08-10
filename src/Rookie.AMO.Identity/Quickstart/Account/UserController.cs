8e94bcf921150555b1e08093789a05336066d2fe
khoadangquoc <quockhoathcstq@gmail.com>
2021-08-11 02:54:30 +0700

    Add GetById in controller

--- src/Rookie.AMO.Identity/Quickstart/Account/UserController.cs
+++ src/Rookie.AMO.Identity/Quickstart/Account/UserController.cs
@@ -44,5 +44,12 @@ namespace Rookie.AMO.Identity.Quickstart.Account
             }
             return Ok("Update successfully");
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+            return Ok(user);
+        }
     }
 }
