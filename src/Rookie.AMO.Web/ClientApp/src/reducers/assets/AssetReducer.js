b5c25ff2435860b1178e1a52e9c96c708a81d183
nhannguyen2882 <62823878+nhannguyen2882@users.noreply.github.com>
2021-08-11 12:07:32 -0700

    modify_reducer

--- src/Rookie.AMO.Web/ClientApp/src/reducers/assets/AssetReducer.js
+++ src/Rookie.AMO.Web/ClientApp/src/reducers/assets/AssetReducer.js
@@ -10,7 +10,7 @@ function AssetReducer(state = initialState, action) {
             return [...state, payload];
 
         case types.FETCH_ASSETS:
-            return payload.data;
+            return payload;
 
         default:
             return state;
