e843c1bb87cef7016cdaa6cd5ccf34df7e1f3a0c
khoadangquoc <quockhoathcstq@gmail.com>
2021-08-13 12:37:23 +0700

    Update

--- src/Rookie.AMO.Web/ClientApp/src/reducers/categoryReducer.js
+++ src/Rookie.AMO.Web/ClientApp/src/reducers/categoryReducer.js
@@ -1,17 +1,17 @@
-import *as types from "../actions/ManagerCategory/ActionType";
+import * as types from "../actions/ManagerCategory/ActionType";
 
 const initialState = [];
 
 function CategoryReducer(state = initialState, action) {
-    const { type, payload } = action;
+  const { type, payload } = action;
 
-    switch (type) {
-        case types.FETCH_CATEGORY:
-            return payload;
-       
-        default:
-            return state;
-    }
-};
+  switch (type) {
+    case types.FETCH_CATEGORY:
+      return payload;
+
+    default:
+      return state;
+  }
+}
 
 export default CategoryReducer;
