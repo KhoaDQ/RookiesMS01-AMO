4d3c1dbf0f61bcd40ae6ce56b0284f60a211c22c
nhannguyen2882 <62823878+nhannguyen2882@users.noreply.github.com>
2021-08-17 11:40:32 -0700

    Add controller API assignment

---
+++ src/Rookie.AMO.Contracts/Dtos/Assignment/AssignmentRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rookie.AMO.Contracts.Dtos.Assignment
+{
+    public class AssignmentRequest
+    {
+        public Guid User_ID { get; set; }
+        public Guid Asset_ID { get; set; }
+        public DateTime AssignedDate { get; set; }
+        public string Note { get; set; }
+        
+    }
+}
---
+++ src/Rookie.AMO.Web/Controllers/AssignmentController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.AspNetCore.Mvc;
+using Rookie.AMO.Business.Interfaces;
+using Rookie.AMO.Contracts;
+using Rookie.AMO.Contracts.Constants;
+using Rookie.AMO.Contracts.Dtos.Assignment;
+
+namespace Rookie.AMO.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentController : ControllerBase 
+    {
+        private readonly IAssignmentService _assignmentService;
+        public AssignmentController(IAssignmentService assignmentService)
+        {
+            _assignmentService = assignmentService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AssignmentDto>> CreateAsync([FromBody] AssignmentRequest assignmentRequest)
+        {
+            Ensure.Any.IsNotNull(assignmentRequest, nameof(assignmentRequest));
+            var assignment = await _assignmentService.AddAsync(assignmentRequest);
+            return Created(Endpoints.Assignment, assignment);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateAsync(Guid id, [FromBody] AssignmentUpdateRequest request)
+        {
+            Ensure.Any.IsNotNull(request, nameof(request));
+            await _assignmentService.UpdateAsync(id,request);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAssignmentAsync([FromRoute] Guid id)
+        {
+            var assignmentDto = await _assignmentService.GetByIdAsync(id);
+            Ensure.Any.IsNotNull(assignmentDto, nameof(assignmentDto));
+            await _assignmentService.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AssignmentDto> GetByIdAsync(Guid id)
+            => await _assignmentService.GetByIdAsync(id);
+
+        [HttpGet]
+        public async Task<IEnumerable<AssignmentDto>> GetAsync()
+            => await _assignmentService.GetAllAsync();
+
+
+        [HttpGet("find")]
+        public async Task<PagedResponseModel<AssignmentDto>> FindAsync([FromQuery] FilterAssignmentsModel filterAssignmentsModel)
+        {
+            return await _assignmentService.PagedQueryAsync(filterAssignmentsModel);
+        }
+    }
+}
