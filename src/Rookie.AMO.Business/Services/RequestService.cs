a02adba1fe0579f7a0942d85624dcaa75e880825
KhoaDQ <quockhoathcstq@gmail.com>
2021-08-18 19:13:20 +0700

    Initial API for request

---
+++ src/Rookie.AMO.Business/Services/RequestService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Rookie.AMO.Business.Extensions;
+using Rookie.AMO.Business.Interfaces;
+using Rookie.AMO.Contracts;
+using Rookie.AMO.Contracts.Dtos.Request;
+using Rookie.AMO.DataAccessor.Data;
+using Rookie.AMO.DataAccessor.Entities;
+using Rookie.AMO.DataAccessor.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rookie.AMO.Business.Services
+{
+    public class RequestService : IRequestService
+    {
+        private readonly IBaseRepository<Request> _baseRepository;
+        private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _context;
+
+        public RequestService(IBaseRepository<Request> baseRepository, IMapper mapper, ApplicationDbContext context)
+        {
+            _baseRepository = baseRepository;
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<PagedResponseModel<RequestDto>> PagedQueryAsync(FilterRequestsModel filter)
+        {
+            // 1. Select Join
+            var query = from r in _context.Requests
+                        join a in _context.Assets on r.AssetID equals a.Id
+                        select new { r, a };
+            // 2. Filter
+            if (!string.IsNullOrEmpty(filter.State))
+            {
+                IEnumerable<int> stateFilter = filter.State.Trim().Split(',').Select(s => EnumConverExtension.GetValueInt<StateList>(s));
+
+                query = query.Where(x => stateFilter.Contains(((int)x.r.State)));
+            }
+
+
+            if (!string.IsNullOrEmpty(filter.OrderProperty))
+                query = query.OrderByPropertyName(filter.OrderProperty, filter.Desc);
+
+            // 3. Paging
+            var requests = await query
+                .PaginateAsync(filter.Page, filter.Limit);
+
+            return new PagedResponseModel<RequestDto>
+            {
+                CurrentPage = requests.CurrentPage,
+                TotalPages = requests.TotalPages,
+                TotalItems = requests.TotalItems,
+                Items = _mapper.Map<IEnumerable<RequestDto>>(requests.Items)
+            };
+        }
+    }
+}
---
+++ src/Rookie.AMO.Contracts/Dtos/Request/RequestDto.cs
@@ -0,0 +1,22 @@
+﻿using Rookie.AMO.DataAccessor.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rookie.AMO.Contracts.Dtos.Request
+{
+    public class RequestDto
+    {
+        public Guid Id { get; set; }
+        public DateTime AssignedDate { get; set; }
+        public DateTime AcceptedDate { get; set; }
+        public DateTime ReturnedDate { get; set; }
+        public StateList State { get; set; }
+        public string Note { get; set; }
+        public Guid AssetID { get; set; }
+        public Guid User_ID { get; set; }
+        public Guid Admin_ID { get; set; }
+    }
+}
