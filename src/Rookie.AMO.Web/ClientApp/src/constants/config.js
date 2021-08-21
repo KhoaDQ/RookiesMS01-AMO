305fa56c652ad04944c4a1304ae3b4a5d353ffcb
KhoaDQ <quockhoathcstq@gmail.com>
2021-08-21 19:39:39 +0700

    fix connection string

--- src/Rookie.AMO.Web/ClientApp/src/constants/config.js
+++ src/Rookie.AMO.Web/ClientApp/src/constants/config.js
@@ -1,16 +1,13 @@
 const origin_azure = 'https://batch2group1.azurewebsites.net/';
-const identity_azure= 'https://batch2group1-idp.azurewebsites.net/';
+const identity_azure = 'https://batch2group1-idp.azurewebsites.net/';
+
+const origin = "https://localhost:5011/"
+const identity = "https://localhost:5001/"
 
 export const IDENTITY_URL_AZURE = identity_azure;
 export const API_URL_AZURE = origin_azure + 'api';
 export const BASE_URL_AZURE = origin_azure;
 
- const origin = "https://localhost:5011/"
- const identity ="https://localhost:5001/"
-
- export const API_URL = origin+ 'api';
- export const BASE_URL = origin;
- export const IDENTITY_URL = identity;
 
 export const Method = {
   Get: 'get',
